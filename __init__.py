c5368034414afc650d6d374526b32919fdb2a5f3
jtb <james.baker@vanderbilt.edu>
2023-05-04 17:25:59 -0500

    refactor: Added custom loglevel and switch to OOP design
    Added a custom log level to the logger called VERBOSE. This falls in between INFO and DEBUG. This level will allow normal runtime information to be displayed but also for verbose info to be displayed if desired. This change was accomplished by Wisperwind's solution (https://stackoverflow.com/questions/2183233/how-to-add-a-custom-loglevel-to-pythons-logging-facility/35804945). Now the create_logger method and get_logger methods are staticmethods
    
    BREAKING CHANGE:

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-from .logger import configure, create_logger, get_logger, get_loglevel, record_inputs
+from .logger import CustomLogger
--- logger.py
+++ logger.py
@@ -1,100 +1,142 @@
 import logging
-import os
 from pathlib import Path
-from typing import Dict
+from typing import Any, Dict
 
-level_dict: Dict[int, int] = {
-    1: logging.DEBUG,
-    2: logging.INFO,
-}
-
-
-def record_inputs(logger, **kwargs) -> None:
-    """function to record the user arguments that were passed to the
-    program. Takes a logger and then a dictionary of the user
-    arguments"""
-
-    logger.setLevel(20)
-
-    for parameter, value in kwargs.items():
-        logger.info(f"{parameter}: {value}")
-
-    # getting the correct log level to reset the logger
-    logger.setLevel(get_loglevel(kwargs["loglevel"]))
+# We are going to configure a specific logging level for
+# VERBOSE versus INFO
+logging.VERBOSE = logging.INFO - 5
 
 
-def get_loglevel(loglevel: int) -> int:
-    """Function that will return a log level based on the input
+class CustomLogger(logging.getLoggerClass()):
+    def __init__(self, name: str, level: int = logging.NOTSET) -> None:
+        """Initialize the CustomLogger class
 
-    Parameters
-    ----------
-    loglevel : int
-        integer that represents what loglevel the program
-        will use. This number will be zero if the user didn't
-        pass the verbose flag and will be 1 if the user
-        passed the verbose flag.
+        Parameters
+        ----------
+        name : str
+            name of the logger. could be __main__
 
-    Returns
-    -------
-    int
-        returns an integer representing the loglevel in the level_dict
-    """
+        level : int
+            log level represented as an integer
+        """
+        super().__init__(name, level)
+        # we want a default verbosity level of 15
 
-    return level_dict.get(loglevel, logging.INFO)
+        logging.addLevelName(logging.VERBOSE, "VERBOSE")
+
+    def verbose(self, msg, *args, **kwargs) -> None:
+        """log messages at a verbose level
+
+        Parameters
+        ----------
+        msg : str
+            message provided to the logger
+        """
+        if self.isEnabledFor(logging.VERBOSE):
+            self._log(logging.VERBOSE, msg, args, **kwargs)
+
+    def record_inputs(self, **kwargs: Dict[str, Any]) -> None:
+        """function to record the user arguments that were passed to the
+        program. Takes a logger and then a dictionary of the user
+        arguments
+
+        Parameters
+        ----------
+        logger : logging.Logger"""
+
+        # changing the loglevel so that it records the info messages here
+        self.setLevel(logging.INFO)
+
+        # going over each value in the kwargs to write to a file
+        for parameter, value in kwargs.items():
+            self.info(f"{parameter}: {value}")
+
+        # getting the correct log level to reset the logger
+        self.setLevel(CustomLogger.get_loglevel(kwargs["loglevel"]))
+
+    @staticmethod
+    def get_loglevel(loglevel: int) -> int:
+        """Function that will return a log level based on the input
+
+        Parameters
+        ----------
+        loglevel : int
+            integer that represents what loglevel the program
+            will use. This number will be zero if the user didn't
+            pass the verbose flag and will be 1 if the user
+            passed the verbose flag.
+
+        Returns
+        -------
+        int
+            returns an integer representing the loglevel in the level_dict
+        """
+        if loglevel == 0:
+            return logging.INFO
+        elif loglevel == 1:
+            return logging.VERBOSE
+        else:
+            return logging.DEBUG
+
+    def configure(
+        self,
+        output: Path,
+        filename: str = "test.log",
+        verbosity: int = 0,
+        to_console: bool = False,
+        format_str: str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    ) -> None:
+        """Function that will configure the logger
+
+        Parameters
+        ----------
+        logger"""
+
+        filename = output / filename
+
+        self.setLevel(CustomLogger.get_loglevel(verbosity))
+
+        file_formatter = logging.Formatter(format_str)
+
+        # program defaults to log to a file called IBDCluster.log in the
+        # output directory
+        fh = logging.FileHandler(filename, mode="w")
+        fh.setFormatter(file_formatter)
+        self.addHandler(fh)
+
+        # If the user selects to also log to console then the program will
+        # log information to the stderr
+        if to_console:
+            stream_formatter = logging.Formatter("%(message)s")
+
+            sh = logging.StreamHandler()
+            sh.setFormatter(stream_formatter)
+            self.addHandler(sh)
+
+    @staticmethod
+    def get_logger(module_name: str, main_name: str = "__main__") -> logging.Logger:
+        """Function that will be responsible for getting the logger for modules"""
+        return logging.getLogger(main_name).getChild(module_name)
+
+    @staticmethod
+    def create_logger(
+        logger_name: str = "__main__",
+    ) -> logging.Logger:
+        """Function that will get the correct logger for the program
+
+        Parameters
+        ----------
+        loglevel : str
+            logging level that the user wants to use. The default level is INFO
+
+        Returns
+        -------
+        logging.Logger
+        """
+
+        logger = logging.getLogger(logger_name)
+
+        return logger
 
 
-def configure(
-    logger: logging.Logger,
-    output: Path,
-    filename: str = "IBDCluster.log",
-    loglevel: int = 0,
-    to_console: bool = False,
-    format_str: str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-) -> None:
-    """Function that will configure the level of logging"""
-
-    filename = output / filename
-
-    logger.setLevel(level_dict.get(loglevel, logging.INFO))
-
-    file_formatter = logging.Formatter(format_str)
-
-    # program defaults to log to a file called IBDCluster.log in the
-    # output directory
-    fh = logging.FileHandler(filename, mode="w")
-    fh.setFormatter(file_formatter)
-    logger.addHandler(fh)
-
-    # If the user selects to also log to console then the program will
-    # log information to the stderr
-    if to_console:
-        stream_formatter = logging.Formatter("%(message)s")
-
-        sh = logging.StreamHandler()
-        sh.setFormatter(stream_formatter)
-        logger.addHandler(sh)
-
-
-def get_logger(module_name: str, main_name: str = "__main__") -> logging.Logger:
-    """Function that will be responsible for getting the logger for modules"""
-    return logging.getLogger(main_name).getChild(module_name)
-
-
-def create_logger(
-    logger_name: str = "__main__",
-) -> logging.Logger:
-    """Function that will get the correct logger for the program
-
-    Parameters
-    ----------
-    loglevel : str
-        logging level that the user wants to use. The default level is INFO
-
-    Returns
-    -------
-    logging.Logger
-    """
-
-    logger = logging.getLogger(logger_name)
-
-    return logger
+logging.setLoggerClass(CustomLogger)
